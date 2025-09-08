94fc9ef8a17af845d5c738cd6039338e9187c24a
Surma Begam <sbtisha2001@gmail.com>
2025-09-08 17:44:03 +0600

    added the cart

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
- const loadCategories=()=> {
-  fetch("https://openapi.programming-hero.com/api/categories") // সার্ভার থেকে ডাটা আনবে
-    .then(res => res.json())      // ডাটাকে JSON বানাবে
-    .then(data =>showCategories(data.categories)) // ডাটা দেখাবে
+    let  cartItems = [];
+     let total = 0; 
+
+const loadCategories=()=> {
+  fetch("https://openapi.programming-hero.com/api/categories") 
+    .then(res => res.json())    
+    .then(data =>showCategories(data.categories)) 
     //.catch(err => console.log("Error:", err));
 }
 
@@ -10,10 +13,10 @@ const showCategories=(titles)=> {
             levelConteiner.innerHTML = "";
 
             for (let title of titles){              
-  const btndiv = document.createElement("div");
-    btndiv.innerHTML=`
+             const btndiv = document.createElement("div");
+               btndiv.innerHTML=`
                            <button onclick="cardConteiner('${title.category_name}')"
-                            class="w-full rounded-2xl bg-yellow-300 py-2 px-12 font-semibold hover:bg-yellow-500 ">
+                            class=" w-full rounded-2xl bg-yellow-300 py-2 px-12 font-semibold hover:bg-yellow-500 ">
                               ${title.category_name}
                                </button> `;      
           
@@ -25,7 +28,6 @@ const showCategories=(titles)=> {
   fetch("https://openapi.programming-hero.com/api/plants")
     .then(res => res.json())
     .then(data => {
-      // প্রথম ৬টা নাও
       const firstSix = data.plants.slice(0, 6);
       dispalay(firstSix);
     });
@@ -33,11 +35,11 @@ const showCategories=(titles)=> {
 
   const cardConteiner = (categoryName) => {
       fetch(`https://openapi.programming-hero.com/api/plants`) // সার্ভার থেকে ডাটা আনবে
-    .then(res => res.json())      // ডাটাকে JSON বানাবে
+    .then(res => res.json())    
     .then((data) => {
                   const filtered = data.plants.filter(plant => plant.category === categoryName);
       dispalay(filtered);
- }) 
+ }); 
   };
  
  const dispalay = (crads) => {
@@ -61,17 +63,55 @@ const showCategories=(titles)=> {
             <div class="badge badge-outline text-xs">${card.category}</div>
             <div class="badge badge-outline text-xs">৳${card.price}</div>
             <button 
-                   class="w-full rounded-xl bg-yellow-300 py-2 px-12 font-semibold hover:bg-yellow-500">
+                   class="add-to-cart w-full rounded-xl bg-yellow-300 py-2 px-12 font-semibold hover:bg-yellow-500">
                        Add to Cart      
                                </button>
           </div>
         </div>
       </div> 
     `;
-          
-    cardConteiner.append(carddiv);
+
+      const btn = carddiv.querySelector(".add-to-cart");
+    btn.addEventListener("click", () => addToCart(card));
+
+       cardConteiner.append(carddiv);
  })       
  };
+    
+     const addToCart = (plant) => {
+          cartItems.push(plant);
+          total +=plant.price;
+          updateCartUI();        
+     };
+     const updateCartUI = () => {
+  const cartList = document.getElementById("cart");
+  const totalSpan = document.getElementById("total");
+
+  cartList.innerHTML = "";
+  cartItems.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${item.name} - ৳${item.price} 
+      <button onclick="removeFromCart(${index})"
+       class="text-red-500 ml-2"><i class="fa-solid fa-xmark"></i>
+       </button>
+    `;
+    cartList.appendChild(li);
+  });
+
+  totalSpan.textContent = total;
+};
+
+const removeFromCart = (index) => {
+  total -= cartItems[index].price;
+  cartItems.splice(index, 1);
+  updateCartUI();
+};
+
+
+
+
+
 loadDefaultPlants();
 loadCategories();
 
